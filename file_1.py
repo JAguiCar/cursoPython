43b2f3e95f9adfa25347310c6d651c902f6b69cb
Jaquelin Aguilar <jaguilar@stengineeringsolutions.com>
2018-01-08 11:39:33 -0600

    My First Hello

---
+++ file_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#------------------------
+__author__ = "Jaqueline Aguilar"
+__copyright__ = "Copyright 2018, ST Project"
+__credits__ = ["Jaqueline Aguilar"]
+__license__ = "GPL 2"
+__mainteiner__ = "JAC"
+__email__ = "jaguilarstengineeringsolutions.com"
+__status__ = "Debug"
+__execution__ "python codel.py"
+#------------------------
+#Library
+import random
+
+#------------------------
+#Code
+def mifirstHello():
+    printf("HolaMundo")
+#------------------------
+#Execution
+if __name__ == "__main__":
+    mifirstHello()
+#------------------------
